8a35564511d74f9fdc82a660a3cb9cc0fcfedceb
Vinit Rai <learner.vinit@gmail.com>
2018-12-14 15:12:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var empty =[];
+  for(var i=0;i<musicians.length;i++){
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return empty;
+}
+function johnLennonFacts(array){
+  var empty=[];
+  while(array.length>0){
+    empty.push(`${array[i] !!!}`);
+  }
+  return empty;
+}
