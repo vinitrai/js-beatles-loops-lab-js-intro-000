42e0ac097b594957eec917ba4384c0255798085c
Vinit Rai <learner.vinit@gmail.com>
2018-12-15 01:21:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var empty =[];
+  for(var i=0;i<musicians.length;i++){
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return empty;
+}
+function johnLennonFacts(array){
+  var num=0;
+  while(num < array.length){
+    array[num]=array[num]+'!!!';
+    num++;
+  }
+  return array;
+}
+function iLoveTheBeatles(num){
+  var empty =[];
+  do{
+    empty.push(`I love the Beatles!`);
+    num = num+1;
+  }while(num<15);
+  return empty;
+}
